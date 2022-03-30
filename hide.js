959dbb339244984a5b0e37b88842dcdf97b60860
loventheair <82316261+loventheair@users.noreply.github.com>
2022-03-30 18:21:56 +0530

    Update hide.js

--- hide.js
+++ hide.js
@@ -1,6 +1,6 @@
 document.ready(function() {
   if (window.location.href.indexOf('https://www.learnfullform.com/')) {
     //Hide the element.
-    jQuery('#main.main.section').hide();
+    jQuery('.main.section').hide();
   }
 });

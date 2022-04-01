a094db644efc1e4cada98ea2027438925021b6a9
loventheair <82316261+loventheair@users.noreply.github.com>
2022-04-01 17:24:44 +0530

    Create tools.js

---
+++ tools.js
@@ -0,0 +1,5 @@
+$("#HTML13").click(function() {
+    $(".poptoolbx").fadeIn()
+}), $(".wcSafeClose").click(function() {
+    $(".poptoolbx").fadeOut(), $("#generatelink").addClass("hidden"), $("#generateurl").val("")
+});
